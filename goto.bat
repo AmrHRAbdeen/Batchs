e5360dce754b7a5330e4954a6f481a9c93f4ea5a
AmrAbdeen <amr.abdeen2050@gmail.com>
2018-12-08 16:09:46 +0200

    Adding more commands

--- goto.bat
+++ goto.bat
@@ -1,12 +1,19 @@
 @echo off
+CLS
 rem "Taking vars values from Terminal as arguments" 
-rem "%1 first argument" 
-rem  "%2 second argument"
-rem  "%3 third argument..upto %9 only"
+rem "%1 first argument:" 
+rem  "%2 second argument:"
+rem  "%3 third argument..[upto %9 only]"
+rem "If you forget to insert one argument it will print an ERR! message"
+IF [%1]== [] GOTO BLANK
+IF [%2]== [] GOTO BLANK
+IF [%3]== [] GOTO BLANK
+		
 set folder_var=%1 
 set file_var=%2  
 set text_var=%3  
 
+	
 IF EXIST %folder_var% ( 
 goto folder
 )
@@ -16,6 +23,8 @@ cd %folder_var%
 echo %text_var% > %file_var%
 rem "Make sure to return to the original folder"
 cd ..
+:BLANK
+	echo ERR:INVALID_INPUT
 pause
 
 
