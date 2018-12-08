f1ebc5d49be6e1db0000770b83e372f2bcf86fac
AmrAbdeen <amr.abdeen2050@gmail.com>
2018-12-08 15:30:31 +0200

    Adding more commands

--- IF_In_BATCH.bat
+++ IF_In_BATCH.bat
@@ -3,14 +3,15 @@ rem "Notepad++ shortcuts"
 rem "CTRL+Shift" >> Move line
 rem "CTRL+D" >> douplicate line (make sure the cursor is on the line)
 rem "ALT+Shift and drag " >> make a muliline cursor 
+set /p folder_var=Please,Enter Folder Name: 
 set /p file_var=Please,Enter file name: 
-set /p text_var=Please,Enter text context:   
+set /p text_var=Please,Enter text context:  
 
 rem "make sure there's a space between <dir_name> and ( " 
-IF NOT EXIST myDir ( 
-mkdir New_Folder
+IF NOT EXIST %folder_var% ( 
+mkdir %folder_var%
 )
-cd New_Folder
+cd %folder_var%
 echo %text_var% > %file_var%
 rem "Make sure to return to the original folder"
 cd ..
