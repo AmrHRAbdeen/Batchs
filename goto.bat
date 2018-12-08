cb361ce35aec4b628599cdf28130a53a52b708e8
AmrAbdeen <amr.abdeen2050@gmail.com>
2018-12-08 16:25:39 +0200

    Adding more commands

--- goto.bat
+++ goto.bat
@@ -5,10 +5,10 @@ rem "%1 first argument:"
 rem  "%2 second argument:"
 rem  "%3 third argument..[upto %9 only]"
 rem "If you forget to insert one argument it will print an ERR! message"
-IF [%1]== [] GOTO BLANK_ONE
-IF [%2]== [] GOTO BLANK_TWO
-IF [%3]== [] GOTO BLANK_THREE
-		
+IF [%1]== [] GOTO BLANK
+IF [%2]== [] GOTO BLANK
+IF [%3]== [] GOTO BLANK
+rem "Optimization:You can check if %3 is found or not and that's enough!"		
 set folder_var=%1 
 set file_var=%2  
 set text_var=%3  
@@ -24,13 +24,8 @@ echo %text_var% > %file_var%
 rem "Make sure to return to the original folder"
 cd ..
 GOTO END
-:BLANK_ONE
-	echo ERR:INVALID_INPUT_1
-:BLANK_TWO
-	echo ERR:INVALID_INPUT_2
-:BLANK_THREE
-	echo ERR:INVALID_INPUT_3
-
+:BLANK
+	echo ERR:INVALID_INPUT
 :END
 pause
 
