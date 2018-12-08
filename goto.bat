0f74ff6e8508afd24c1ea006099ce89161ef4ee2
AmrAbdeen <amr.abdeen2050@gmail.com>
2018-12-08 16:22:49 +0200

    Adding more commands

--- goto.bat
+++ goto.bat
@@ -5,9 +5,9 @@ rem "%1 first argument:"
 rem  "%2 second argument:"
 rem  "%3 third argument..[upto %9 only]"
 rem "If you forget to insert one argument it will print an ERR! message"
-IF [%1]== [] GOTO BLANK
-IF [%2]== [] GOTO BLANK
-IF [%3]== [] GOTO BLANK
+IF [%1]== [] GOTO BLANK_ONE
+IF [%2]== [] GOTO BLANK_TWO
+IF [%3]== [] GOTO BLANK_THREE
 		
 set folder_var=%1 
 set file_var=%2  
@@ -23,8 +23,15 @@ cd %folder_var%
 echo %text_var% > %file_var%
 rem "Make sure to return to the original folder"
 cd ..
-:BLANK
-	echo ERR:INVALID_INPUT
+GOTO END
+:BLANK_ONE
+	echo ERR:INVALID_INPUT_1
+:BLANK_TWO
+	echo ERR:INVALID_INPUT_2
+:BLANK_THREE
+	echo ERR:INVALID_INPUT_3
+
+:END
 pause
 
 
