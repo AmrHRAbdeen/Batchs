c36a294312681acbcd48e0e7e33a5a3c250f471a
Amr Abdeen <amr.abdeen2050@gmail.com>
2021-09-04 12:51:48 +0200

    Create Get_WiFi_Info.bat

---
+++ Batch_Scripts/Get_WiFi_Info.bat
@@ -0,0 +1,6 @@
+:: This Command will display WiFi info.
+>> netsh wlan show profiles
+:: This Command will display the password of a specific network
+>> netsh wlan show profile <WiFi_Name> key=clear
+
+
